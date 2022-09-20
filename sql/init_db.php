4e54c8a3da54ee7cc8080225c4a099ef1b8383c7
Gginne <23042760+Gginne@users.nonreply.github.com>
2022-09-20 16:38:30 -0400

    Created sql configuration and user table

--- sql/init_db.php
+++ sql/init_db.php
@@ -6,56 +6,26 @@ error_reporting(E_ALL);
 //pull in db.php so we can access the variables from it
 require_once (__DIR__ . "/../config/db.php");
 $count = 0;
+
 try{
     foreach(glob(__DIR__ . "/*.sql") as $filename){
         $sql[$filename] = file_get_contents($filename);
     }
+
     if(isset($sql) && $sql && count($sql) > 0){
-        /***
-         * Sort the array so queries are executed in anticipated order.
-         * Be careful with naming, 1-9 is fine, 1-10 has 10 run after #1 due to string sorting.
-         */
-        ksort($sql);
+   
+    
         echo "<br><pre>" . var_export($sql, true) . "</pre><br>";
+
         //connect to DB
         $db = getDB();
-        /***
-         * Let's make this function a bit smarter to save DB calls for small dev plans (i.e., heroku)
-         */
         $stmt = $db->prepare("show tables");
         $stmt->execute();
         $count++;
-        $tables = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        $t = [];
-        //convert it to a flat array
-        foreach($tables as $row){
-            foreach($row as $key => $value) {
-                array_push($t, $value);
-            }
-        }
         
         foreach($sql as $key => $value){
             echo "<br>Running: " . $key;
-            $lines = explode("(", $value, 2);
-            if(count($lines) > 0){
-                //these lines attempt to extract the table info from any create commands
-                //to determine if they command should be skipped or not
-                $line = $lines[0];
-                //clear out duplicate whitespace
-                $line = preg_replace('!\s+!', ' ', $line);
-                //remove create table command
-                $line = str_ireplace("create table", "", $line);
-                //remove if not exists command
-                $line = str_ireplace("if not exists", "", $line);
-                //remove backticks
-                $line = str_ireplace("`","",$line);
-                //trim whitespace in front and back
-                $line = trim($line);
-                if (in_array($line, $t)){
-                    echo "<br>Blocked from running, table found in 'show tables' results.<br>";
-                    continue;
-                }
-            }
+    
             $stmt = $db->prepare($value);
             $result = $stmt->execute();
             $count++;
