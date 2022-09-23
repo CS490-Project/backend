7f341e2cc4fe1c3ef03f0e7070296b3c5536eacf
Gginne <23042760+Gginne@users.nonreply.github.com>
2022-09-23 00:02:51 -0400

    Created user getter

---
+++ users/get_user.php
@@ -0,0 +1,18 @@
+<?php require_once(__DIR__ . "/../config/db.php"); ?>
+
+<?php
+
+    $userId = NULL;
+
+    if (isset($_POST['userId'])){
+
+
+        $db = getDB();
+
+        $stmt = $db->prepare('SELECT * FROM cs490_users WHERE userId=:userId');
+        $stmt->execute([
+            ":userId" => $_POST['userId']
+        ]);
+    
+        $results = $stmt->fetch(PDO::FETCH_ASSOC);
+    }
