9b6afc78ce6a7b37290b6137c42a654668ab4a8f
Gginne <23042760+Gginne@users.nonreply.github.com>
2022-09-28 23:08:06 -0400

    Final touched to alpha backend

--- users/get_user.php
+++ users/get_user.php
@@ -1,17 +1,16 @@
 <?php require_once(__DIR__ . "/../config/db.php"); ?>
-
 <?php
 
     $userId = NULL;
 
-    if (isset($_POST['userId'])){
+    if (isset($_POST['ucid'])){
 
 
         $db = getDB();
 
-        $stmt = $db->prepare('SELECT * FROM cs490_users WHERE userId=:userId');
+        $stmt = $db->prepare('SELECT * FROM cs490_users WHERE ucid=:ucid');
         $stmt->execute([
-            ":userId" => $_POST['userId']
+            ":ucid" => $_POST['ucid']
         ]);
     
         $results = $stmt->fetch(PDO::FETCH_ASSOC);
@@ -23,6 +22,7 @@
         } else{
             http_response_code(404);
             echo "User not found";
+            die();
         }
         
     }
