2faeea6d3f4fe4c3281f3edd34ba4d813d046bb0
Gginne <23042760+Gginne@users.nonreply.github.com>
2022-10-09 03:01:11 -0400

    Finished controller to create questions

--- questions.php/create_question.php
+++ questions.php/create_question.php
@@ -1,44 +1,43 @@
 <?php require_once(__DIR__ . "/../config/db.php"); ?>
 <?php
+    
+    $json = file_get_contents('php://input');
   
-    if (isset($_POST['description'])  && isset($_POST['difficulty']) 
-        && isset($_POST['category'])  && isset($_POST['teacher_id'])
-        && isset($_POST['fname']) ){
+    if ($json){
 
-
+        $data = json_decode($json, true);
         $db = getDB();
         
 
         $stmt = $db->prepare('INSERT INTO cs490_questions (description, difficulty, fname, category, teacher_id) VALUES(:desc, :diff, :fn, :cat, :t_id)');
         $r = $stmt->execute([
-            ":desc" => $_POST['description'],
-            ":diff" => $_POST['difficulty'],
-            ":fn" => $_POST['fname'],
-            ":cat" => $_POST['category'],
-            ":t_id" => $_POST['teacher_id']
+            ":desc" => $data['description'],
+            ":diff" => $data['difficulty'],
+            ":fn" => $data['fname'],
+            ":cat" => $data['category'],
+            ":t_id" => $data['teacher_id']
         ]);
     
 
         if($r){
             $q_id = $db->lastInsertId();
-            $test_cases = $_POST['test_cases'];
+            $test_cases = $data['test_cases'];
 
             foreach ($test_cases as $tc) {
                 $stmt = $db->prepare('INSERT INTO cs490_test_cases (test_in, test_out, question_id) VALUES(:t_out, :t_in, :q_id)');
                 $r = $stmt->execute([
                     ":t_in" => $tc['test_in'],
                     ":t_out" => $tc['test_out'],
-                    ":q_id" => $_id
+                    ":q_id" => $q_id
                 ]);
 
                 if($r){
-                    echo "created Questiona dn Testcases";
+                    echo "Created Testcase\n";
                 } else {
-                    echo "Failed to Create Question";
+                    echo "Failed to Create Testcase";
                     http_response_code(500);
                     die();
                 }
-    
                 
             }
         } else{
